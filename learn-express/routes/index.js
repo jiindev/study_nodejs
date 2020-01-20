5b61cf77e7399cfd26997fdc71d0e6e9e4e1719d
jiindev <jiindev@gmail.com>
2020-01-20 23:52:02 +0900

    mysql과 시퀄라이즈 실습 완료

--- learn-express/routes/index.js
+++ learn-express/routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-app.get('/', (req, res)=>{
+router.get('/', (req, res, next)=>{
   console.log('세번째 미들웨어');
-  res.send('Hello express');
+  res.render('test', {
+    title:'express2',
+    title2:'hello',
+    fruits:['사과', '배', '오렌지']
+  });
 });
 
-app.post('/', (req, res)=>{
+router.post('/', (req, res)=>{
 
 });
 
-app.delete('/users', (req, res)=>{
+router.delete('/users', (req, res)=>{
 
 });
 
