b8ff35740aa7e6ce2d0bb8b64c99b8a8bfd1dcb3
jiindev <jiindev@gmail.com>
2020-01-28 15:29:02 +0900

    간단한 cli 프로그램 완성

--- node-cli/index.js
+++ node-cli/index.js
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
-console.log('Hello CLI', process.argv);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.clear();
+const answerCallBack = (answer) =>{
+    if(answer === 'y'){
+        console.log('수고하시네요.');
+        rl.close();
+    }else if (answer === 'n'){
+        console.log('한번 해보세요.');
+        rl.close();
+    }else{
+        console.clear();
+        console.log('y 또는 n만 입력하세요.');
+        rl.question('cli 예제를 하고 계신가요? (y/n)', answerCallBack);
+    }
+}
+
+
+rl.question('cli 예제를 하고 계신가요? (y/n)', answerCallBack);
