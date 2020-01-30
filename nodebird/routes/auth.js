bacd60cb6125b891778f6a0bd3a882919cdef208
jiindev <jiindev@gmail.com>
2020-01-30 16:04:18 +0900

    로그인 로그아웃 구현

--- nodebird/routes/auth.js
+++ nodebird/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router;
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 const {User} = require('../models');
+const passport = require('passport');
 const bcrypt = require('bcrypt');
 
-router.post('/join', (req, res, next)=>{
+const router = express.Router;
+
+router.post('/join', isNotLoggedIn, async(req, res, next)=>{
     const {email, nick, password} = req.body;
     try{
         const exUser = await User.find({where: {email}});
@@ -26,8 +29,30 @@ router.post('/join', (req, res, next)=>{
     }
 });
 
-router.post('/login', (req, res, next)=>{
-
+router.post('/login', isNotLoggedIn, (req, res, next)=>{ //req.body.email, req.body.password
+    passport.authenticate('local', (authError, user, info)=>{
+        if(authError){
+            console.error(authError);
+            return next(authError);
+        }
+        if(!user){
+            req.flash('loginError', info.message);
+            return res.redirect('/');
+        }
+        return req.login(user, (loginError)=>{
+            if(loginError){
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        })
+    })(req, res, next);
 });
+
+router.get('/logout', isLoggedIn, (req, res)=>{
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+})
 
 module.exports = router;
